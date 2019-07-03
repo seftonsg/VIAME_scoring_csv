ea36fce7472b9e8855f3f8b6a17e0c4091ef9328
Sage <seftonsg@icloud.com>
2019-07-02 17:45:43 -0700

    Added real data and cleanup.  run.bat is a template.

---
+++ run.bat
@@ -0,0 +1,6 @@
+python.exe expander.py ^
+	-truth tracks/truths/TRUTHS_5img.csv ^
+	-computed tracks/5img/1c_tracks_CRNN_v0915_5img.csv ^
+	-images 5images.txt ^
+	-script score_joint.bat ^
+	-output real_data/1c_CRNN
